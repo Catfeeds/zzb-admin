6ac758efb62c982e9c0e4442a1d2e0c6faa6f429
hhc <hhc@cto100.com>
2017-08-24 23:52:45 +0800

    1

--- src/main/java/com/hcb/zzb/dto/Car.java
+++ src/main/java/com/hcb/zzb/dto/Car.java
@@ -468,6 +468,15 @@ public class Car {
 	public void setModelYear(String modelYear) {
 		this.modelYear = modelYear;
 	}
+	
+	private String userName;
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
     
     
 }
